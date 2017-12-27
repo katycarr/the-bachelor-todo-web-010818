ec8922fa4cd585423dc97a80319c715fd9f88e01
Katy Carr <khcarr9@gmail.com>
2017-12-27 19:21:42 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -43,6 +43,13 @@ end
 
 def get_occupation(data, hometown)
   # code here
+  data.each do |season, info|
+    info.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
